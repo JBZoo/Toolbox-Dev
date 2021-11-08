11ed5273e96dc3395958c09907dec60ea8b1629e
Denis Smetannikov <smet.denis@gmail.com>
2021-11-08 13:55:58 +0200

    Updated deps

--- tests/ToolboxDevReadmeTest.php
+++ tests/ToolboxDevReadmeTest.php
@@ -33,20 +33,8 @@ class ToolboxDevReadmeTest extends AbstractReadmeTest
     {
         parent::setUp();
 
-        $this->params['scrutinizer'] = true;
         $this->params['codefactor'] = true;
         $this->params['strict_types'] = true;
-    }
-
-    /**
-     * @return string|null
-     */
-    protected function checkBadgeTravis(): ?string
-    {
-        return $this->getPreparedBadge($this->getBadge(
-            'Build Status',
-            'https://travis-ci.org/__VENDOR_ORIG__/__PACKAGE_ORIG__.svg?branch=master',
-            'https://travis-ci.org/__VENDOR_ORIG__/__PACKAGE_ORIG__'
-        ));
+        $this->params['travis'] = true;
     }
 }
